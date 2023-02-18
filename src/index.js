a620c12d4d347dbdfdd151490a7a07121d9ac6cc
Oleh Kliapko <goldclub1977@gmail.com>
2023-02-18 17:56:18 +0200

    create project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { ColorModeScript } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <StrictMode>
+    <ColorModeScript />
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
+
+serviceWorker.unregister();
